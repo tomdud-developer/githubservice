d83a676a38c38c68461829683533bb12d4d2ecf8
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-24 20:03:48 +0200

    Initial commit - Spring initializer

---
+++ src/main/java/com/tomdud/githubservice/GithubserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tomdud.githubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomdud/githubservice/GithubserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomdud.githubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
