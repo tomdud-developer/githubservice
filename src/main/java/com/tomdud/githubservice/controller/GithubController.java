0586d13c72e2f3e59cb2f18dfbc7ad6f3852638c
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-25 11:44:24 +0200

    Wrap controller response

--- src/main/java/com/tomdud/githubservice/controller/GithubController.java
+++ src/main/java/com/tomdud/githubservice/controller/GithubController.java
@@ -1,13 +1,18 @@
 package com.tomdud.githubservice.controller;
 
+import com.tomdud.githubservice.dto.ApiResponse;
 import com.tomdud.githubservice.dto.RepositoryResponseDTO;
 import com.tomdud.githubservice.service.GithubService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("github/repositories")
@@ -17,7 +22,13 @@ public class GithubController {
     private final GithubService githubService;
 
     @GetMapping("/{username}")
-    public Flux<RepositoryResponseDTO> getUserNotForkedRepositoriesInformation(@PathVariable String username) {
-        return githubService.getUserNotForkedRepositoriesInformation(username);
+    public Mono<ResponseEntity<ApiResponse<List<RepositoryResponseDTO>>>> getUserNotForkedRepositoriesInformation(@PathVariable String username) {
+        return githubService.getUserNotForkedRepositoriesInformation(username)
+                .collectList()
+                .map(response -> ApiResponse.<List<RepositoryResponseDTO>>builder()
+                        .status("success")
+                        .results(response)
+                        .build()
+                ).map(ResponseEntity::ok);
     }
 }
--- src/main/java/com/tomdud/githubservice/dto/ApiResponse.java
+++ src/main/java/com/tomdud/githubservice/dto/ApiResponse.java
@@ -1,4 +1,20 @@
 package com.tomdud.githubservice.dto;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class ApiResponse<T> {
+    private String status;
+    private List<ErrorDTO> errors;
+    private T results;
 }
