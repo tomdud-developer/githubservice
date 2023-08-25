7405ef3e41591d78db621abcb19b700bdf42f49c
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-25 11:25:55 +0200

    Write simple controller

--- src/main/java/com/tomdud/githubservice/controller/GithubController.java
+++ src/main/java/com/tomdud/githubservice/controller/GithubController.java
@@ -1,4 +1,23 @@
 package com.tomdud.githubservice.controller;
 
+import com.tomdud.githubservice.dto.RepositoryResponseDTO;
+import com.tomdud.githubservice.service.GithubService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("github/repositories")
+@AllArgsConstructor
 public class GithubController {
+
+    private final GithubService githubService;
+
+    @GetMapping("/{username}")
+    public Flux<RepositoryResponseDTO> getUserNotForkedRepositoriesInformation(@PathVariable String username) {
+        return githubService.getUserNotForkedRepositoriesInformation(username);
+    }
 }
